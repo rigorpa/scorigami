84a6d161d5bab9e3e95bfadbd572682eff69cce2
rigorpa <orozco.rigo@gmail.com>
2026-06-30 11:24:08 -0700

    v0.5.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.scorigami.app"
         minSdk = 30
         targetSdk = 35
-        versionCode = 26
-        versionName = "0.5.6"
+        versionCode = 27
+        versionName = "0.5.7"
     }
 
     buildTypes {
